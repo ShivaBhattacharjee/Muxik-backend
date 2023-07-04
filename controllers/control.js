d15199c310ac0004828c72a8f142e4529d48f09d
Shiva <shivaxwarrior@gmail.com>
2023-07-04 15:42:20 +0530

    Fixed registration bugs

--- controllers/control.js
+++ controllers/control.js
@@ -1,63 +1,41 @@
-import userModel from "../models/user.model.js"
-import bcrypt from "bcrypt"
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
 // post method register route
 export async function register(req, res) {
     try {
-        const [username, password, profile, email] = req.body
-
-        // checking if user or email already exists
-
-        const existUserName = new Promise((resolve, reject) => [
-            userModel.findOne({ username }, function (err, user) {
-                if (err) reject(new Error(err))
-                if (user) reject({ error: "Username already exists" })
-
-                resolve();
-            })
-        ])
-
-
-        const existEmail = new Promise((resolve, reject) => [
-            userModel.findOne({ email }, function (err, email) {
-                if (err) reject(new Error(err))
-                if (email) reject({ error: "An account is already associated witht this email" })
-
-                resolve();
-            })
-        ]);
-
-        Promise.all([existUserName, existEmail]).then(() => {
-            if (password) {
-                bcrypt.hash(password, 10).then(hashedPassword => {
-                    const user = new userModel({
-                        username,
-                        password: hashedPassword,
-                        profile: profile || "",
-                        email
-                    }).catch(error => {
-                        return res.status(500).send({
-                            error: "unable to hash password"
-                        })
-                    })
-
-                    user.save().then(result => res.status(201).send({
-                        message: "User Registration successful"
-                    })).catch(error => {
-                        res.status(500).send({
-                            message: "Unable to register user"
-                        })
-                    })
-                })
-            }
-        }).catch(error => {
-            return res.status(500).send({
-                message : "Error unable to save data"
-            })
-        })
+        const { username, password, profile, email } = req.body;
+        
+        // Check if the username or email already exists in the database
+        
+        const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+        if (existingUser) {
+            return res.status(400).send({
+                message: "Username or email already exists"
+            });
+        }
+        
+        // If the username and email are unique, proceed with user registration
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            username,
+            password: hashedPassword,
+            profile: profile || "",
+            email
+        });
+        
+        const savedUser = await newUser.save();
+        
+        res.status(201).send({
+            message: "User registration successful",
+            user: savedUser
+        });
     } catch (error) {
         return res.status(500).send({
-            message : "Error in register route"
-        })
+            message: "Error in register route",
+            error: error.message
+        });
     }
 }
 
