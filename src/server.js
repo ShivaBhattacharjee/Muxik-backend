d276e5c46c1a0e6875f8c333feaedd4349ecf9b8
Shiva <shivaxwarrior@gmail.com>
2023-07-08 13:14:41 +0530

    Added DELETE AS CORS METHOD

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     "origin": "*",
-    "methods": "GET,PUT,POST",
+    "methods": "GET,PUT,POST,DELETE",
 }))
 app.use(morgan('tiny'))
 app.disable('x-powered-by')
