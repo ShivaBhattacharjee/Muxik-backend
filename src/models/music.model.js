b44ecf36bf80d1ca64ba0efb4e070d0e998a6ae0
Shiva <shivaxwarrior@gmail.com>
2023-07-15 20:13:09 +0530

    Added schema for liked songs

--- src/models/music.model.js
+++ src/models/music.model.js
@@ -1,1 +1,17 @@
-// monggo db schema to store history liked songs and playlist
+// mongo db schema to store history liked songs and playlist
+import mongoose from "mongoose";
+
+export const LikedSongSchema = new mongoose.Schema({
+  songId: {
+    type: String,
+    required: true,
+  },
+  songName: {
+    type: String,
+    required: true,
+  },
+  banner: {
+    type: String,
+    required: true,
+  },
+});
