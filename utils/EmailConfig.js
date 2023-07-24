8811779ad39b9ed2ff777b14ca50ecabc796e439
Shiva <shivaxwarrior@gmail.com>
2023-07-24 15:24:58 +0530

    edited email config

--- utils/EmailConfig.js
+++ utils/EmailConfig.js
@@ -2,8 +2,6 @@ import nodemailer from "nodemailer";
 
 // Function to create and return a nodemailer transporter
 export function createTransporter() {
-  console.log("Emailid" + process.env.EMAIL_ID)
-  console.log("EmailPassword" + process.env.EMAIL_PASSWORD)
   return nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
