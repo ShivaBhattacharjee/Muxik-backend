700c6f5eb74e651d85d2470f2f464a07b145aec0
Shiva <shivaxwarrior@gmail.com>
2023-08-03 01:47:58 +0530

    removed unused packages

--- src/controllers/control.js
+++ src/controllers/control.js
@@ -1,6 +1,3 @@
-import User from "../models/user.model.js";
-import bcrypt from "bcrypt";
-import otpGenerator from "otp-generator";
 import { createTransporter } from "../../utils/EmailConfig.js";
 import emailTemplate from "../../utils/Template/RegisterEmail.js";
 import passwordResetTemplate from "../../utils/Template/ForgotPassword.js";
