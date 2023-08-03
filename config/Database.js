6339f90047f0f25ccc9511f6017c177533247d45
Shiva <shivaxwarrior@gmail.com>
2023-08-03 16:44:05 +0530

    Connected real database and added example data

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,8 @@
 // mongo db connection
 import mongoose from "mongoose";
-import { MongoMemoryServer } from 'mongodb-memory-server';
 async function connect(){
-    const mongodb = await  MongoMemoryServer.create()
-    const getUri = mongodb.getUri(process.env.MONGO_DB_URI)
-
     mongoose.set('strictQuery', true)
-    const db = await mongoose.connect(getUri)
+    const db = await mongoose.connect(process.env.MONGO_DB_URI)
     if(db){
         console.log("🥳 Databse created successfully")
     }else{
